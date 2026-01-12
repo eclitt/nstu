d864192b2010dbc09f0adacdb72e9b50b55fee19
eclitt <vladtutrin095@gmail.com>
2026-01-12 14:08:45 +0700

    123

---
+++ 1sem/programming/train/1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Company {
+static int count;
+private:
+    string name;
+    int profit;
+};
+
+
+int main() { 
+    Company nstu;
+}
